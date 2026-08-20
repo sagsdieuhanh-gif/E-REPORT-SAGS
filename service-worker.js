5785b44e816ef1e7badf62d984eeeb258074b4b7
sagsdieuhanh-gif <sagsdieuhanh@gmail.com>
2026-08-20 12:45:23 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="sags-v1-90-toolbar-click-fix-20260820-01";
-const BUILD="V1.90-20260820-01";
+const CACHE_NAME="sags-v1-92-dh-quicktime-20260820-01";
+const BUILD="V1.92-20260820-01";
 const APP_SHELL=[
   "./index.html","./version.json","./firebase-config.js","./daily-roster.js","./v488-archive.js","./ai-crosscheck.js","./document-scanner.js","./admin-builder.js","./ac-limits.js","./ac-limits-simple.js","./bbbt-quick-entry.js","./roster-completed.js",
   "./alert-mva.mp3","./alert-mvt.mp3","./alert-pushback-missing.mp3","./alert-read-sign.mp3","./alert-ket-so-moi.mp3","./alert-ket-so-thay-doi.mp3",
