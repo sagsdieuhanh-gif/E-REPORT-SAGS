e5b414fea0cdf7b3dc1a2bb71505875120c1d855
sagsdieuhanh-gif <sagsdieuhanh@gmail.com>
2026-08-22 19:09:37 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="sags-v3-78-admin-form-tools-20260822-01";
-const BUILD="V3.78-20260822-01";
+const CACHE_NAME="sags-v3-79-admin-tools-closeout-clean-20260822-01";
+const BUILD="V3.79-20260822-01";
 const APP_SHELL=[
   "./9Gfinal.png",
   "./CBTT.png",
